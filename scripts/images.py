3dd39036a8a712e466509cf2e5d8669e3e42b237
neov2 <neov2@illinois.edu>
2020-06-01 16:11:38 -0400

    new labeled images and annotations

--- scripts/images.py
+++ scripts/images.py
@@ -18,6 +18,35 @@ def flip_images():
         # flip image and save
         im = Image.open("../training/unlabeled/" + file)
         flipped = ImageOps.mirror(im)
-        flipped.save("../training/unlabeled-flipped/" + file[ : index] + "-f.jpg")
+        flipped.save("../training/unlabeled/" + file[ : index] + "-f.jpg")
+
+# resizes and rotates images to the same dimensions
+def resize_images():
+    """
+    GOAL: take all images in unlabeled training data and rotate and resize them 
+    to be the same shape and size (510x385)
+    """
+    # get all files from unlabeled data set
+    filenames = [files for path, dirs, files in os.walk("../training/unlabeled/")][0]
+
+    # rotate image if needed, resize them, then save them
+    for file in filenames:
+        # open image and get dimensions
+        image = Image.open("../training/unlabeled/" + file)
+        width, height = image.size
+
+        # rotate image if the height is larger than width
+        if height > width:
+            image = image.transpose(Image.ROTATE_90)
+
+        # resize image to 510x385
+        image = image.resize((510, 385))
+        
+        # save image
+        image.save("../training/unlabeled/" + file)
 
 flip_images()
+resize_images()
+
+
+
