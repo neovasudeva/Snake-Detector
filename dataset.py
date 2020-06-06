1940e952975ab8b23d014bbd48ade401497a6443
neov2 <neov2@illinois.edu>
2020-06-06 01:55:37 -0400

    made loading dataset more variable, starting to create network

--- dataset.py
+++ dataset.py
@@ -5,9 +5,12 @@ import cv2
 import tqdm
 import random
 
+from detectron2 import model_zoo
 from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.structures import BoxMode
 from detectron2.utils.visualizer import Visualizer
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
 
 # global constants for labels
 labels_dict = {0: "snake", 1: "background"}
@@ -155,24 +158,33 @@ for d in ["train", "test"]:
     MetadataCatalog.get("snake_" + d).set(thing_classes=["snake"])
 
 # check data
-test_dataset("./train/labeled/")
+# test_dataset("./train/labeled/")
 
+# Configs for Mask R-CNN X101-FPN
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"))
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml")
+cfg.DATASETS.TRAIN = ("snake_train", )
+cfg.DATASETS.TEST = ("snake_test", )
+cfg.DATALOADER.NUM_WORKERS = 4                  # threads for data loading
+cfg.SOLVER.IMS_PER_BATCH = 4
+cfg.SOLVER.BASE_LR = 0.0005                     # change me
+cfg.SOLVER.MAX_ITER = 500                       # may need to increase later
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 64   # may need to increase later
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1             # 1 class (snake)
 
+# load outputs into "output" directory
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+print("Training is starting...")
+trainer.train()
+print("Training is finished!")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# save the model
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "snake_detector.pth")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+predictor = DefaultPredictor(cfg)
 
 
 
